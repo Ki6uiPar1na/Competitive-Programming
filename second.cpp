538a1cd698595576dfd7e4fa5113880523d67a71
Md. Khairul Islam <khairulislamtushar33@gmail.com>
2024-06-07 18:58:25 +0600

    This is a simple test for my git

---
+++ second.cpp
@@ -0,0 +1,27 @@
+/*____________________________________________________
+|Author: Ki6ui-Par1na
+|Date: 2024/06/07
+|Time: 18:56:30
+|Problem: second
+|____________________________________________________*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define print(n) cout << (n) << endl;
+#define tc ll t; cin >> t; while(t--)
+#define int long long
+void solve() {
+    cout << "Again Test\n";
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    solve();
+
+    return 0;
+}
