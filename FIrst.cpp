a2cf377afddd50aef3dd9d074a6f6d95a481d7a8
Md. Khairul Islam <khairulislamtushar33@gmail.com>
2024-06-07 18:53:40 +0600

    First Program

---
+++ FIrst.cpp
@@ -0,0 +1,27 @@
+/*____________________________________________________
+|Author: Ki6ui-Par1na
+|Date: 2024/06/07
+|Time: 18:53:11
+|Problem: FIrst
+|____________________________________________________*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define print(n) cout << (n) << endl;
+#define tc ll t; cin >> t; while(t--)
+#define int long long
+void solve() {
+    cout << "New" << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    solve();
+
+    return 0;
+}
